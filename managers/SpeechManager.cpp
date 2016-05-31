caa3e346ba3fa123ff88a153adc19602ecb427e1
Aimed <blablab@blabla.com>
2016-05-31 20:56:05 +0200

    Убрал кривой код. Дополнение к предидущему коммиту: добавил инклюд хешмапы в stdafx.h и обьявление поля мапы локалей-векторов в SpeechManager.h

--- managers/SpeechManager.cpp
+++ managers/SpeechManager.cpp
@@ -80,10 +80,9 @@ bool TSpeechManager::LoadSpeech()
 			str.erase(str.begin());
 		}
 
-		my_map[str] = 1;
 		item.Data = str;
 
-		TPRINT("[0x%04X]=(len=%i, cs=%i, ce=%i) %s\n", item.Code, len, item.CheckStart, item.CheckEnd, ToString(str).c_str());
+		//TPRINT("[0x%04X]=(len=%i, cs=%i, ce=%i) %s\n", item.Code, len, item.CheckStart, item.CheckEnd, ToString(str).c_str());
 	}
 
 	return true;
