6b6432328d0e4dd4006784240f7cfa05e6559dd4
Hotride <la2sw@list.ru>
2016-09-17 13:06:42 +0300

    Поправил цвет поля для ввода текста в CGumpGeneric.

--- OrionUO/GUI/GUIGenericTextEntry.cpp
+++ OrionUO/GUI/GUIGenericTextEntry.cpp
@@ -10,7 +10,7 @@
 #include "GUIGenericTextEntry.h"
 //----------------------------------------------------------------------------------
 CGUIGenericTextEntry::CGUIGenericTextEntry(const uint &serial, const uint &index, const ushort &color, const int &x, const int &y, const int &maxWidth, const int &maxLength)
-: CGUITextEntry(serial, color, 0, 0, x, y, maxWidth, true, 0, TS_LEFT, UOFONT_BLACK_BORDER, maxLength),
+: CGUITextEntry(serial, color, color, color, x, y, maxWidth, true, 0, TS_LEFT, UOFONT_BLACK_BORDER, maxLength),
 m_TextID(index)
 {
 }
