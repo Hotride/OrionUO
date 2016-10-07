23a92d2b32c779c9056d93bd2d5508b07e9e37cd
Тимур Мустакимов <mustakimov@ocean.local>
2016-10-07 17:35:10 +0300

    Приведение интерфейсов к стандартным типам.

--- OrionUO/CommonInterfaces.cpp
+++ OrionUO/CommonInterfaces.cpp
@@ -28,70 +28,70 @@ void __cdecl FUNCBODY_PopScissor()
 	g_GL.PopScissor();
 }
 //----------------------------------------------------------------------------------
-void __cdecl FUNCBODY_DrawLine(uint color, int x, int y, int width, int height)
+void __cdecl FUNCBODY_DrawLine(unsigned int color, int x, int y, int width, int height)
 {
 	glColor4ub(GetRValue(color), GetGValue(color), GetBValue(color), (color >> 24) & 0xFF);
 	g_GL.DrawLine(x, y, width, height);
 	glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
 }
 //----------------------------------------------------------------------------------
-void __cdecl FUNCBODY_DrawPolygone(uint color, int x, int y, int width, int height)
+void __cdecl FUNCBODY_DrawPolygone(unsigned int color, int x, int y, int width, int height)
 {
 	glColor4ub(GetRValue(color), GetGValue(color), GetBValue(color), (color >> 24) & 0xFF);
 	g_GL.DrawPolygone(x, y, width, height);
 	glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
 }
 //----------------------------------------------------------------------------------
-void __cdecl FUNCBODY_DrawCircle(uint color, float x, float y, float radius, int gradientMode)
+void __cdecl FUNCBODY_DrawCircle(unsigned int color, float x, float y, float radius, int gradientMode)
 {
 	glColor4ub(GetRValue(color), GetGValue(color), GetBValue(color), (color >> 24) & 0xFF);
 	g_GL.DrawCircle(x, y, radius, gradientMode);
 	glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
 }
 //----------------------------------------------------------------------------------
-void __cdecl FUNCBODY_DrawTextA(int x, int y, uchar font, const char *text, ushort color, int width, TEXT_ALIGN_TYPE align, ushort flags)
+void __cdecl FUNCBODY_DrawTextA(int x, int y, unsigned char font, const char *text, unsigned short color, int width, TEXT_ALIGN_TYPE align, unsigned short flags)
 {
 	g_FontManager.DrawA(font, text, color, x, y, width, align, flags);
 }
 //----------------------------------------------------------------------------------
-void __cdecl FUNCBODY_DrawTextW(int x, int y, uchar font, const wchar_t *text, ushort color, int width, TEXT_ALIGN_TYPE align, ushort flags)
+void __cdecl FUNCBODY_DrawTextW(int x, int y, unsigned char font, const wchar_t *text, unsigned short color, int width, TEXT_ALIGN_TYPE align, unsigned short flags)
 {
 	g_FontManager.DrawW(font, text, color, x, y, 30, width, align, flags);
 }
 //----------------------------------------------------------------------------------
-void __cdecl FUNCBODY_DrawArt(int x, int y, ushort graphic, ushort color)
+void __cdecl FUNCBODY_DrawArt(int x, int y, unsigned short graphic, unsigned short color)
 {
 	g_Orion.DrawStaticArt(graphic, color, x, y, 0);
 }
 //----------------------------------------------------------------------------------
-void __cdecl FUNCBODY_DrawArtAnimated(int x, int y, ushort graphic, ushort color)
+void __cdecl FUNCBODY_DrawArtAnimated(int x, int y, unsigned short graphic, unsigned short color)
 {
 	g_Orion.DrawStaticArtAnimated(graphic, color, x, y, 0);
 }
 //----------------------------------------------------------------------------------
-void __cdecl FUNCBODY_DrawResizepicGump(int x, int y, ushort graphic, int width, int height)
+void __cdecl FUNCBODY_DrawResizepicGump(int x, int y, unsigned short graphic, int width, int height)
 {
 	g_Orion.DrawResizepicGump(graphic, x, y, width, height);
 }
 //----------------------------------------------------------------------------------
-void __cdecl FUNCBODY_DrawGump(int x, int y, ushort graphic, ushort color)
+void __cdecl FUNCBODY_DrawGump(int x, int y, unsigned short graphic, unsigned short color)
 {
 	g_Orion.DrawGump(graphic, color, x, y);
 }
 //----------------------------------------------------------------------------------
-void __cdecl FUNCBODY_DrawGumppic(int x, int y, ushort graphic, ushort color, int width, int height)
+void __cdecl FUNCBODY_DrawGumppic(int x, int y, unsigned short graphic, unsigned short color, int width, int height)
 {
 	g_Orion.DrawGump(graphic, color, x, y, width, height);
 }
 //----------------------------------------------------------------------------------
 //IUltimaOnline
 //----------------------------------------------------------------------------------
-uint __cdecl FUNCBODY_GetLandFlags(ushort graphic)
+unsigned int __cdecl FUNCBODY_GetLandFlags(unsigned short graphic)
 {
 	return g_Orion.GetLandFlags(graphic);
 }
 //----------------------------------------------------------------------------------
-uint __cdecl FUNCBODY_GetStaticFlags(ushort graphic)
+unsigned int __cdecl FUNCBODY_GetStaticFlags(unsigned short graphic)
 {
 	return g_Orion.GetStaticFlags(graphic);
 }
@@ -108,17 +108,17 @@ void __cdecl FUNCBODY_SetConfigValue(const char *option, int value)
 //----------------------------------------------------------------------------------
 //IClilocManager
 //----------------------------------------------------------------------------------
-std::string __cdecl FUNCBODY_GetClilocA(uint clilocID, std::string defaultText)
+std::string __cdecl FUNCBODY_GetClilocA(unsigned int clilocID, std::string defaultText)
 {
 	return g_ClilocManager.Cliloc(g_Language)->GetA(clilocID, defaultText);
 }
 //----------------------------------------------------------------------------------
-std::wstring __cdecl FUNCBODY_GetClilocW(uint clilocID, std::string defaultText)
+std::wstring __cdecl FUNCBODY_GetClilocW(unsigned int clilocID, std::string defaultText)
 {
 	return g_ClilocManager.Cliloc(g_Language)->GetW(clilocID, defaultText);
 }
 //----------------------------------------------------------------------------------
-std::wstring __cdecl FUNCBODY_GetClilocArguments(uint clilocID, std::wstring args)
+std::wstring __cdecl FUNCBODY_GetClilocArguments(unsigned int clilocID, std::wstring args)
 {
 	return g_ClilocManager.ParseArgumentsToClilocString(clilocID, args);
 }
@@ -130,44 +130,44 @@ int __cdecl FUNCBODY_GetHuesCount()
 	return g_ColorManager.GetHuesCount();
 }
 //----------------------------------------------------------------------------------
-ushort __cdecl FUNCBODY_GetColor32To16(uint &color)
+unsigned short __cdecl FUNCBODY_GetColor32To16(unsigned int &color)
 {
 	return g_ColorManager.Color32To16(color);
 }
 //----------------------------------------------------------------------------------
-uint __cdecl FUNCBODY_GetColor16To32(ushort &color)
+unsigned int __cdecl FUNCBODY_GetColor16To32(unsigned short &color)
 {
 	return g_ColorManager.Color16To32(color);
 }
 //----------------------------------------------------------------------------------
-ushort __cdecl FUNCBODY_GetColorToGray(ushort &color)
+unsigned short __cdecl FUNCBODY_GetColorToGray(unsigned short &color)
 {
 	return g_ColorManager.ConvertToGray(color);
 }
 //----------------------------------------------------------------------------------
-uint __cdecl FUNCBODY_GetPolygoneColor(ushort c, ushort color)
+unsigned int __cdecl FUNCBODY_GetPolygoneColor(unsigned short c, unsigned short color)
 {
 	return g_ColorManager.GetPolygoneColor(c, color);
 }
 //----------------------------------------------------------------------------------
-uint __cdecl FUNCBODY_GetColor(ushort &c, ushort color)
+unsigned int __cdecl FUNCBODY_GetColor(unsigned short &c, unsigned short color)
 {
 	return g_ColorManager.GetColor(c, color);
 }
 //----------------------------------------------------------------------------------
-uint __cdecl FUNCBODY_GetHartialHueColor(ushort &c, ushort color)
+unsigned int __cdecl FUNCBODY_GetHartialHueColor(unsigned short &c, unsigned short color)
 {
 	return g_ColorManager.GetPartialHueColor(c, color);
 }
 //----------------------------------------------------------------------------------
 //IPathFinder
 //----------------------------------------------------------------------------------
-bool __cdecl FUNCBODY_GetCanWalk(uchar &direction, int &x, int &y, char &z)
+bool __cdecl FUNCBODY_GetCanWalk(unsigned char &direction, int &x, int &y, char &z)
 {
 	return g_PathFinder.CanWalk(direction, x, y, z);
 }
 //----------------------------------------------------------------------------------
-bool __cdecl FUNCBODY_GetWalk(bool run, uchar direction)
+bool __cdecl FUNCBODY_GetWalk(bool run, unsigned char direction)
 {
 	return g_PathFinder.Walk(run, direction);
 }
