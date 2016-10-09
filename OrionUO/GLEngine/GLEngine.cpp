ffb207f8e94075967b7bdd9703bb853c91cd8d1e
AimedNuu <valkaenator@gmail.com>
2016-10-09 03:22:17 +0200

    Added glPixelStorei unpack alignment right before texture uploading. An attempt to fix AMD videocards crash.

--- OrionUO/GLEngine/GLEngine.cpp
+++ OrionUO/GLEngine/GLEngine.cpp
@@ -165,8 +165,6 @@ void CGLEngine::UpdateRect()
 //----------------------------------------------------------------------------------
 void CGLEngine::BindTexture16(GLuint &texture, const int &width, const int &height, const pushort pixels)
 {
-	glPixelStorei(GL_PACK_ALIGNMENT, 1);
-	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
@@ -174,14 +172,13 @@ void CGLEngine::BindTexture16(GLuint &texture, const int &width, const int &heig
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	DEBUGLOG("\t_bt16...create (%ix%i) 0x%08X\n", width, height, pixels);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_BGRA, GL_UNSIGNED_SHORT_1_5_5_5_REV, pixels);
 	DEBUGLOG("\t_bt16...end\n");
 }
 //----------------------------------------------------------------------------------
 void CGLEngine::BindTexture32(GLuint &texture, const int &width, const int &height, const puint pixels)
 {
-	glPixelStorei(GL_PACK_ALIGNMENT, 1);
-	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 	glGenTextures(1, &texture);
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
@@ -190,7 +187,8 @@ void CGLEngine::BindTexture32(GLuint &texture, const int &width, const int &heig
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
 	DEBUGLOG("\t_bt32...create (%ix%i) 0x%08X\n", width, height, pixels);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8, pixels);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8, pixels);
 	DEBUGLOG("\t_bt32...end\n");
 }
 //----------------------------------------------------------------------------------
