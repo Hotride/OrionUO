9e57d85da7368cb24a3694a3c76212955ca55a48
Тимур Мустакимов <mustakimov@ocean.local>
2016-10-12 16:22:58 +0300

    Подсветка магии в оглавлении книги заклинаний при наведении на нее курсором.

--- OrionUO/GUI/GUIHitBox.cpp
+++ OrionUO/GUI/GUIHitBox.cpp
@@ -8,6 +8,7 @@
 */
 //----------------------------------------------------------------------------------
 #include "GUIHitBox.h"
+#include "../SelectedObject.h"
 //----------------------------------------------------------------------------------
 CGUIHitBox::CGUIHitBox(const uint &serial, const int &x, const int &y, const int &width, const int &height, const bool &callOnMouseUp)
 : CGUIPolygonal(GOT_HITBOX, x, y, width, height, callOnMouseUp), m_ToPage(-1)
@@ -17,5 +18,17 @@ CGUIHitBox::CGUIHitBox(const uint &serial, const int &x, const int &y, const int
 //----------------------------------------------------------------------------------
 CGUIHitBox::~CGUIHitBox()
 {
+}
+//----------------------------------------------------------------------------------
+void CGUIHitBox::OnMouseEnter()
+{
+	if (g_SelectedObject.Gump() != NULL)
+		g_SelectedObject.Gump()->WantRedraw = true;
+}
+//----------------------------------------------------------------------------------
+void CGUIHitBox::OnMouseExit()
+{
+	if (g_LastSelectedObject.Gump() != NULL)
+		g_LastSelectedObject.Gump()->WantRedraw = true;
 }
 //----------------------------------------------------------------------------------
