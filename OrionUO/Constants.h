25615522968443c77c9c6afd30a556da0f5bc9f1
Hotride <la2sw@list.ru>
2016-12-08 00:24:40 +0300

    Обновил максимальный индекс анимаций в соответствии с последним клиентом.

--- OrionUO/Constants.h
+++ OrionUO/Constants.h
@@ -80,7 +80,7 @@ const int MAX_MULTI_DATA_INDEX_COUNT = 0x2200;
 const int MAX_MUSIC_DATA_INDEX_COUNT = 100;
 
 //!Размер таблицы индексов анимаций
-const int MAX_ANIMATIONS_DATA_INDEX_COUNT = 1250;
+const int MAX_ANIMATIONS_DATA_INDEX_COUNT = 2047;
 
 //!Размер таблицы индексов анимаций
 const int MAX_LIGHTS_DATA_INDEX_COUNT = 100;
