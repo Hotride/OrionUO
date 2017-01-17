bded5031186f451377e13c5f90ce36dfd7e55c12
AimedNuu <valkaenator@gmail.com>
2017-01-17 21:11:33 +0100

    Added AutoResetEvent class to solution.

---
+++ OrionUO/AutoResetEvent.h
@@ -0,0 +1,24 @@
+﻿#include <mutex>
+#include <condition_variable>
+
+//http://stackoverflow.com/questions/8538575/is-there-an-easy-way-to-implement-autoresetevent-in-c0x
+//Этим обьектом можно сигналить между тредами.
+//Тред А будет ждать пока Тред Б посигналит через этот обьект об окончании своей работы.
+///
+class AutoResetEvent
+{
+public:
+	explicit AutoResetEvent(bool initial = false);
+
+	void Set();
+	void Reset();
+
+	bool WaitOne();
+
+private:
+	AutoResetEvent(const AutoResetEvent&);
+	AutoResetEvent& operator=(const AutoResetEvent&); // non-copyable
+	bool flag_;
+	std::mutex protect_;
+	std::condition_variable signal_;
+};
