92ceb0c1c1918a25f301e8c294adc696be069d8e
Hotride <la2sw@list.ru>
2017-10-18 00:47:17 +0300

    Revert bad commit with changes in character selection packet.

--- OrionUO/Network/Packets.cpp
+++ OrionUO/Network/Packets.cpp
@@ -193,7 +193,16 @@ CPacketSelectCharacter::CPacketSelectCharacter(const uint &index, const string &
 	WriteUInt8(0x5D);
 	WriteUInt32BE(0xEDEDEDED);
 	WriteString(name.c_str(), 30, false);
-	Move(30);
+	Move(2);
+
+	uint clientFlag = 0;
+
+	IFOR(i, 0, g_CharacterList.ClientFlag)
+		clientFlag |= (1 << i);
+
+	WriteUInt32BE(clientFlag);
+
+	Move(24);
 	WriteUInt32BE(index);
 	WriteDataBE(g_ConnectionManager.GetClientIP(), 4);
 }
