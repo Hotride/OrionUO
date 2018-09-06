a2c641e5d79f95286ad30029ea6e61c601252e97
Danny Angelo Carminati Grein <fungos@gmail.com>
2018-09-05 20:20:16 -0500

    Cleanup CharacterList

--- OrionUO/CharacterList.cpp
+++ OrionUO/CharacterList.cpp
@@ -1,29 +1,11 @@
-﻿// This is an open source non-commercial project. Dear PVS-Studio, please check it.
-// PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
-/***********************************************************************************
-**
-** CharacterList.cpp
-**
-** Copyright (C) August 2016 Hotride
-**
-************************************************************************************
-*/
-//----------------------------------------------------------------------------------
-#include "stdafx.h"
+﻿CCharacterList g_CharacterList;
 
-CCharacterList g_CharacterList;
-//----------------------------------------------------------------------------------
 CCharacterList::CCharacterList()
 {
     WISPFUN_DEBUG("c182_f1");
-    //!Чистим при создании
     Clear();
 }
-//----------------------------------------------------------------------------------
-/*!
-Очистка списка
-@return 
-*/
+
 void CCharacterList::Clear()
 {
     WISPFUN_DEBUG("c182_f2");
@@ -35,51 +17,28 @@ void CCharacterList::Clear()
     Have6Slot = false;
     Have7Slot = false;
 }
-//----------------------------------------------------------------------------------
-/*!
-Установить имя персонажа в указанном слоте
-@param [__in] pos Позиция в списке
-@param [__in] name Новое имя
-@return 
-*/
+
 void CCharacterList::SetName(intptr_t pos, const string &name)
 {
     WISPFUN_DEBUG("c182_f3");
-    //!При корректной позиции записываем имя
     if (pos >= 0 && pos < Count)
         m_Name[pos] = name;
 }
-//----------------------------------------------------------------------------------
-/*!
-Получить имя персонажа в указанном слоте
-@param [__in] pos Позиция в списке
-@return Имя персонажа
-*/
+
 string CCharacterList::GetName(intptr_t pos) const
 {
     WISPFUN_DEBUG("c182_f4");
-    string result = "";
-
-    //!При корректной позиции возвращаем имя
     if (pos >= 0 && pos < Count)
-        result = m_Name[pos];
+        return m_Name[pos];
 
-    return result;
+    return "";
 }
-//----------------------------------------------------------------------------------
-/*!
-Получить имя выбранного персонажа
-@return Имя персонажа
-*/
+
 string CCharacterList::GetSelectedName() const
 {
     WISPFUN_DEBUG("c182_f5");
-    string result = "";
-
-    //!При корректной позиции возвращаем имя
     if (Selected >= 0 && Selected < Count)
-        result = m_Name[Selected];
+        return m_Name[Selected];
 
-    return result;
+    return "";
 }
-//----------------------------------------------------------------------------------
